570ea6cbef9d2574748cb7e7c1a1c35c8349fa79
Ex0318199501242 <luisrs_2008@hotmail.com>
2020-02-12 01:27:24 -0600

    Commit terminar metodo post

--- routes/api/seguridad/seguridad.js
+++ routes/api/seguridad/seguridad.js
@@ -5,3 +5,10 @@ var userModel = require('./seguridad.model');
 router.get('/users/all', (req, res)=>{
     return res.status(200).json(userModel.getAll());
 } );
+
+// http://localhost:3000/api/seguridad/users/new
+router.post('/users/new', (req, res)=>{
+    var datosEnviados = req.body;
+    var newUser = userModel.addNew(datosEnviados);
+    return res.status(200).json(newUser);
+  }); // post users/new
